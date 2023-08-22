635453d77345a8d06f87c307b3b208b59916d644
Mike Misiura <mikemisiura@gmail.com>
2023-08-22 03:34:37 -0600

    changed Calendly

--- src/components/Calendly.tsx
+++ src/components/Calendly.tsx
@@ -14,30 +14,30 @@ function Calendly() {
         smoke
     } = useContext(EstimateContext)
 
-    const answer1:number = seats - 4
-    const a1: string = answer1.toString()
+    // const answer1:number = seats - 4
+    // const a1: string = answer1.toString()
 
-    let a2: string = '1'
-    let a3: string = '1'
-    let a4: string = '1'
-    let a5: string = '1'
+    // let a2: string = '1'
+    // let a3: string = '1'
+    // let a4: string = '1'
+    // let a5: string = '1'
 
-    if (!conditioner) {a3 = '2'}
-    if (!leather) {
-        a2 = '2'
-        a3 = '3'
-    }
-    if (!pets) {a4 = '2'}
-    if (!smoke) {a5 = '2'}
+    // if (!conditioner) {a3 = '2'}
+    // if (!leather) {
+    //     a2 = '2'
+    //     a3 = '3'
+    // }
+    // if (!pets) {a4 = '2'}
+    // if (!smoke) {a5 = '2'}
 
     return (
         <div>
             {/* <br /> */}
             <InlineWidget
-                url="https://calendly.com/mikemisiura"
+                url="https://calendly.com/dinkumdetailing/mobile_service"
                 prefill={{
                     email: email,
-                    customAnswers: { a1, a2, a3, a4, a5 }
+                    // customAnswers: { a1, a2, a3, a4, a5 }
                 }}
                 styles={{
                     height: '650px',
